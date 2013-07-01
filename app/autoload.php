ae999bc672ab7a9ca4744681d66fa041c1e6dac1
Miro Cillik <miro@keboola.com>
2013-07-01 16:24:21 +0200

    Upgrade to Symfony 2.3

--- app/autoload.php
+++ app/autoload.php
@@ -1,15 +1,12 @@
 <?php
 
+use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
+/**
+ * @var $loader ClassLoader
+ */
 $loader = require __DIR__.'/../../../autoload.php';
-
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../../../symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add('', __DIR__.'/../../../symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-}
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
