ebec455f8d206d10e2a32e74a04dd408ed05c8be
Martin Halamíček <martin@halamicek.cz>
2013-12-09 10:24:20 +0100

    Default timezone, disabled debug mode in web/app.php

--- web/app.php
+++ web/app.php
@@ -8,14 +8,7 @@ define('ROOT_PATH', __DIR__.'/../');
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
-Debug::enable();
+date_default_timezone_set('Europe/Prague');
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
